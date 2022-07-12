a04f53663ea84ea56503b3f8c63d0af37ad53798
Herp Derpinstine <herpderpinstine@gmail.com>
2022-07-12 00:10:45 -0500

    Cleanup

--- bHapticsLib/Internal/ThreadSafeQueue.cs
+++ bHapticsLib/Internal/ThreadSafeQueue.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace bHapticsLib
+namespace bHapticsLib.Internal
 {
     internal class ThreadSafeQueue<T> : IEnumerable<T>, IEnumerable, ICollection
     {
