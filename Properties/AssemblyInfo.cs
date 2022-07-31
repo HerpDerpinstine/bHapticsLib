ef5f506abfdb5bd0682bb4857d1fd0145ea58611
Herp Derpinstine <herpderpinstine@gmail.com>
2022-07-31 05:02:05 -0500

    Cleanup

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using bHapticsLib.Properties;
 [assembly: AssemblyProduct(BuildInfo.Name)]
 [assembly: AssemblyCopyright("Created by " + BuildInfo.Author)]
 [assembly: AssemblyTrademark(BuildInfo.Company)]
-[assembly: Guid("068259B1-7730-47D6-B3DF-DADD79AEBC3C")]
+[assembly: Guid("C55CC59C-138B-48DF-95CC-FA956D064600")]
 [assembly: AssemblyVersion(BuildInfo.Version)]
 [assembly: AssemblyFileVersion(BuildInfo.Version)]
 [assembly: NeutralResourcesLanguage("en")]
