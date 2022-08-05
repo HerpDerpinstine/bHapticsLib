e0ba0ae7a8bdf9e1734421833b458202afe0b30d
Herp Derpinstine <herpderpinstine@gmail.com>
2022-08-05 12:38:01 -0500

    Cleanup Preparations for NuGet

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using bHapticsLib.Properties;
 
 [assembly: AssemblyTitle(BuildInfo.Name)]
-[assembly: AssemblyCompany(BuildInfo.Company)]
+[assembly: AssemblyCompany("Lava Gang")]
 [assembly: AssemblyProduct(BuildInfo.Name)]
 [assembly: AssemblyCopyright("Created by " + BuildInfo.Author)]
 [assembly: AssemblyTrademark(BuildInfo.Company)]
