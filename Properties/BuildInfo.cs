dadbbe1abfb72023c5eb838964baaba12a67320e
Herp Derpinstine <herpderpinstine@gmail.com>
2022-08-18 23:09:55 -0500

    Fix NuGet Description

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -5,7 +5,7 @@
         public const string Name = "bHapticsLib";
         public const string Author = "Herp Derpinstine";
         public const string Company = "Lava Gang";
-        public const string Version = "1.0.4";
+        public const string Version = "1.0.3";
         public const string DownloadLink = "https://github.com/HerpDerpinstine/bHapticsLib";
     }
 }
